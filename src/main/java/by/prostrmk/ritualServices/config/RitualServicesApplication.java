61b53e2481ad332e4d5687e9f216d0ddb1d7b5c0
prostfk@gmail.com <prostfk@gmail.com>
2018-07-08 21:14:34 +0300

    init project

---
+++ src/main/java/by/prostrmk/ritualServices/config/RitualServicesApplication.java
@@ -0,0 +1,14 @@
+package by.prostrmk.ritualServices.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = "by.prostrmk.ritualServices")
+public class RitualServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RitualServicesApplication.class, args);
+    }
+}
---
+++ src/main/java/by/prostrmk/ritualServices/controller/MainController.java
@@ -0,0 +1,17 @@
+package by.prostrmk.ritualServices.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public @ResponseBody String getIndex(){
+        return "hello world, loh";
+    }
+
+
+}
